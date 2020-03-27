87786b270bdb980a2ae3ac6e337a3395dca53617
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-03-27 12:35:57 +0100

    cleanup and some comments

--- src/roman-numera.ts
+++ src/roman-numera.ts
@@ -15,6 +15,7 @@ const ROMAN_VALUES = {
 };
 
 const ROMAN_LETTERS = {
+    0: '',
     1: 'I',
     5: 'V',
     10: 'X',
@@ -24,58 +25,53 @@ const ROMAN_LETTERS = {
     1000: 'M',
 };
 
-function toRomanRec(input: number, size: number) {
-    let prefix = '';
-    while(input >= size) {
-        prefix += ROMAN_LETTERS[size];
-        input -= size;
-    }
-    if (size <= 1)
-        return prefix + (ROMAN_LETTERS[input] || '');
-    if (input === 0)
-        return prefix;
+/** Returns the roman char for a numeric value */
+const romanChar = (value: number) => ROMAN_LETTERS[value];
+/** Returns the roman chars symbolizing the subtraction of the specified values */
+const subtractRomanChar = (value: number, subtractedValue: number) => ROMAN_LETTERS[subtractedValue] + ROMAN_LETTERS[value];
+
+/**
+ * @param input the value to convert
+ * @param size  the highest value we expect to find
+ */
+function toRomanRecursive(input: number, size: number) {
+    if (input >= size)
+        return romanChar(size) + toRomanRecursive(input - size, size);
 
     const halfSize = size/2;
-    if (input === halfSize)
-        return prefix + ROMAN_LETTERS[input];
+    if (size <= 1 || input === halfSize)
+        return romanChar(input);
 
     const nextSize = size/10;
-    const subtractAbove1 = (size - nextSize);
-    if (input >= subtractAbove1) {
-        const mid = ROMAN_LETTERS[nextSize] + ROMAN_LETTERS[size];
-        if (input === subtractAbove1)
-            return prefix + mid;
-        return prefix + mid + toRomanRec(input - subtractAbove1, nextSize);
-    }
-    if (input > halfSize)
-        return prefix + ROMAN_LETTERS[halfSize] + toRomanRec(input - halfSize, nextSize);
+    if (input > halfSize && input < (size - nextSize))
+        return romanChar(halfSize) + toRomanRecursive(input - halfSize, nextSize);
 
-    const subtractAbove2 = (halfSize - nextSize);
-    if (input >= subtractAbove2) {
-        const mid = ROMAN_LETTERS[nextSize] + ROMAN_LETTERS[halfSize];
-        if (input === subtractAbove2)
-            return prefix + mid;
-        return prefix + mid + toRomanRec(input - subtractAbove2, nextSize);
+    for (const sizeFrom of [size, halfSize]) {
+        const subtractAbove = (sizeFrom - nextSize);
+        if (input >= subtractAbove)
+            return subtractRomanChar(sizeFrom, nextSize) + toRomanRecursive(input - subtractAbove, nextSize);
     }
-    return prefix + toRomanRec(input, nextSize);
+    return toRomanRecursive(input, nextSize);
 }
 
 function toRoman(input: number) {
     if (input < 1 || input > 3000)
         fail('Value must be between 1 and 3000');
 
-    return toRomanRec(input, 1000);
+    return toRomanRecursive(input, 1000);
 }
 
 function fromRoman(input: string) {
     let value = 0;
     let rightCharValue = 0;
+    // iterate string in reverse order
     const reversedChars = input.split('').reverse();
     for(const c of reversedChars) {
         const charValue = ROMAN_VALUES[c];
         if(!charValue)
             fail(`Invalid char: ${c}`);
 
+        // subtract the value if right char > current char
         if (rightCharValue > charValue) {
             value -= charValue;
         } else {
@@ -87,8 +83,10 @@ function fromRoman(input: string) {
     return value;
 }
 
+// This is just the command line interpretation:
 const value = getArguments(1)[0];
-if (value === 'all') {
+if (value === 'samples') {
+    // try samples from txt file
     const romans = fs.readFileSync('./roman.txt', 'utf-8').trim().split('\n');
     let failed = false;
     for(let i=0; i<romans.length; i++) {
@@ -107,8 +105,33 @@ if (value === 'all') {
     if (!failed) {
         console.log(`${romans.length} samples match!`);
     }
+} else if (value.includes(':')) {
+    // compare results of fromRoman with toRoman
+    const parts = value.split(':');
+    const from = parseInt(parts[0], 10);
+    const to = parseInt(parts[1], 10);
+    if (isNaN(from) || from < 0  || isNaN(to) || to > 3000 || from > to)
+        fail('Invalid range');
+    let failed = false;
+    const values = [];
+    for(let i=from; i<=to; i++) {
+        const roman = toRoman(i);
+        const romanAsNum = fromRoman(roman);
+        if (romanAsNum !== i) {
+            console.error(`toRoman(${i}) returned "${roman}", but fromRoman("${roman}") returned ${romanAsNum}`);
+            failed = true;
+        } else {
+            values.push(roman);
+        }
+    }
+    if (!failed) {
+        console.log(values.join(', '));
+        console.log(`All values in range ${value} match!`);
+    }
 } else if(IS_NUMERIC.test(value)) {
+    // convert numeric to roman
     console.log(`Roman value of ${value} is ${toRoman(parseInt(value, 10))}`);
 } else {
+    // convert roman to numeric
     console.log(`Numeric value of ${value} is ${fromRoman(value.toUpperCase())}`);
 }
