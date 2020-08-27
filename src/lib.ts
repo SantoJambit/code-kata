664bd4a36c4e0a81771e90aaa05b3dad57382972
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-08-27 16:42:21 +0200

    solution

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,3 @@
-
 export function fail(message: string) {
     console.error(message);
     process.exit(0);
@@ -14,15 +13,24 @@ export function getArguments(count: number) {
 export class StopWatch {
     private creationTime = Date.now();
     private startTime = Date.now();
+    private lastDuration = 0;
+    private currentTask = "";
+
     public start(task: string) {
         this.startTime = Date.now();
-        console.log(task);
+        this.currentTask = task;
     }
+
     public stop() {
-        console.log(` -> ${Date.now() - this.startTime}ms to complete task\n`);
+        this.lastDuration = Date.now() - this.startTime;
+        console.log(`${this.currentTask} -> ${this.lastDuration}ms to complete task\n`);
     }
 
     public finish() {
         console.log(`\n Done => ${Date.now() - this.creationTime}ms to finish\n`);
+    }
+
+    public getLastDuration() {
+        return this.lastDuration;
     }
 }
--- src/wabbit_hunt.ts
+++ src/wabbit_hunt.ts
@@ -1,1 +1,36 @@
-// Todo
+import { Vector, IndexedVector } from './load_hunting_ground';
+
+function cross(a: Vector, b: Vector) {
+    return a.x * b.y - b.x * a.y;
+}
+
+function vectorTo(from: Vector, to: Vector): Vector {
+    return {
+        x: to.x - from.x,
+        y: to.y - from.y,
+    };
+}
+
+export function getBunnyCountOnLine(positions: IndexedVector[], indexA: number, indexB: number) {
+    const posA = positions[indexA];
+    const posB = positions[indexB];
+    const vecAB = vectorTo(posA, posB);
+
+    const positionsOnLine = positions.filter((posC) => {
+        if (posC.index === indexA || posC.index === indexB) return false;
+        const vecAC = vectorTo(posA, posC);
+        return cross(vecAB, vecAC) === 0;
+    });
+    return 2 + positionsOnLine.length;
+}
+
+export function findBestShotForIndex(positions: IndexedVector[], index: number) {
+    const otherPositions = positions.filter((p) => p.index !== index);
+    const bunnyCounts = otherPositions.map((p) => getBunnyCountOnLine(positions, index, p.index));
+    return Math.max(...bunnyCounts);
+}
+
+export function findBestShot(positions: IndexedVector[]) {
+    const bunnyCounts = positions.map((_, i) => findBestShotForIndex(positions, i));
+    return Math.max(...bunnyCounts);
+}
